4dc118f20ee995f9cd59d893fa947385caac7d5b
naragornc <naragorn.dev@gmail.com>
2017-07-29 20:29:23 +0700

    Show data form SignupController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,18 @@
 @extends('templates.template')
 
 @section('content')
+
+  <div class="container">
+    @foreach ($registers as $register)
+    <div class="row">
+      <div class="well">
+        <label>Text</label>
+        <p>{{$register}}</p>
+        <p></p>
+      </div>
+    </div>
+    @endforeach
+  </div>
+
   @include('components.signup')
 @endsection
