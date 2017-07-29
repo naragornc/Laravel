488aaa4aa2a8efd43ebdaf5b752c5ba67deac34b
naragornc <naragorn.dev@gmail.com>
2017-07-29 16:57:04 +0700

    Init sup view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,5 @@
-@extends('template')
+@extends('templates.template')
 
 @section('content')
-  <form method="post" action="/signup">
-    <div class="form-group">
-      <label for="firstname">Firstname</label>
-      <input class="form-control" placeholder="firstname">
-    </div>
-    <div class="form-group">
-      <label for="lastname">Lastname</label>
-      <input class="form-control" placeholder="lastname">
-    </div>
-    <div class="form-group">
-      <input type="submit" class="btn btn-default">
-    </div>
-  </form>
+  @include('components.signup')
 @endsection
